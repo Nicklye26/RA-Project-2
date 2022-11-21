5342fe82ed34cfac772963a4ab95e6ad99fb1f1f
yomansg <br.networkers@gmail.com>
2022-11-21 23:56:26 +0800

    Created composer form for adding a new record

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Composer from "./Composer.js";
 
 function App() {
+  // Code to use later for checking whether user is logged in
+  // const [loggedInUser, setLoggedInUser] = useState();
+
+  // useEffect(() => {
+  //   onAuthStateChanged(auth, (user) => {
+  //     if (user) {
+  //       // User is signed in, saved logged-in user to state
+  //       setLoggedInUser(user);
+  //       return;
+  //     }
+
+  //     // User is signed out
+  //     setLoggedInUser(null);
+  //   });
+  // }, []);
+
+  const composer = <Composer />;
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Routes>
+          <Route path="/" element={composer} />
+        </Routes>
       </header>
     </div>
   );
